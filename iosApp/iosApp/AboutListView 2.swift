410bfdc802c898ff1976911842e9d15ab9649c4b
Iago Ferreira <iago-ferreira@outlook.com>
2025-01-29 04:11:50 -0300

    feat: updated ios native  component

---
+++ iosApp/iosApp/AboutListView 2.swift
@@ -0,0 +1,13 @@
+//
+//  AboutListView 2.swift
+//  iosApp
+//
+//  Created by Iago Ferreira on 1/28/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+
+import Shared
+import SwiftUI
+
+
---
+++ iosApp/iosApp/SwiftUIView.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIView.swift
+//  iosApp
+//
+//  Created by Iago Ferreira on 1/28/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import Shared
+import SwiftUI
+
+
+
+#Preview {
+    SwiftUIView()
+}
